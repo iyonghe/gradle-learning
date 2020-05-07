577a9e923082cc0af43162cc6e704a558564909f
xiaoyuyou <754731025@qq.com>
2020-05-07 09:50:58 +0800

    修改sql语句

--- mysql.sql
+++ mysql.sql
@@ -1,8 +1,12 @@
+create database gradle;
+
+use gradle;
+
 create table users (
     `uid` bigint(20) NOT NULL COMMENT 'uid',
     `nick` varchar(20) NOT NULL COMMENT '昵称',
-    PRIMARY KEY (`uid`),
-) ENGINE=InnoDB DEFAULT COMMENT '用户表';
+    PRIMARY KEY (`uid`)
+) ENGINE=InnoDB COMMENT '用户表';
 
 insert into users(uid, nick) values (1, '张三');
 insert into users(uid, nick) values (2, '李四');
